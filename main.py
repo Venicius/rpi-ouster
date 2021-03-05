f6afc5da669222a6a2818939d623eb42f577d166
Venicius <venicius.alves@gmail.com>
2021-03-05 14:16:15 -0300

    Correçoes sugeridas pelopycharm

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from os1 import OS1
 from imu import OS1_IMU
 from imu.utils import data_imu
 from os1.utils import xyz_points_raw
-import csv
 import os
 from datetime import datetime
 
 
 def handler(raw_packet):
-    filename = 'Raw_' + str(getDateTime()) + '.txt'
+    filename = 'Raw_' + str(getdatetime()) + '.txt'
     print("Generating output in " + filename)
     with open(filename, 'a') as f:
         ch, ch_range, reflectivity, intensity, timeStamp, encoderCount, measurementID, frameID, x, y, z, noise = xyz_points_raw(
@@ -17,17 +16,19 @@ def handler(raw_packet):
         for vetorDadosColetados in zip(ch, ch_range, encoderCount,
                                        reflectivity, intensity, x, y, z,
                                        noise):
-                                       if(ch==0):
-                                           f.write("TimeStamp: " + str(timeStamp[0]) + " measurementID: " + str(measurementID[0]) + " frameID: " + str(frameID[0]) + "\n")
+            if ch == 0:
+                f.write(
+                    "TimeStamp: " + str(timeStamp[0]) + " measurementID: " + str(measurementID[0]) + " frameID: " + str(
+                        frameID[0]) + "\n")
 
-            linhaImpressaNoArquivo = str(vetorDadosColetados)
-            linhaImpressaNoArquivo = linhaImpressaNoArquivo.replace(',', ' ')
-            linhaImpressaNoArquivo = linhaImpressaNoArquivo.replace('(', ' ')
-            linhaImpressaNoArquivo = linhaImpressaNoArquivo.replace(')', ' ')
-            f.write(linhaImpressaNoArquivo + "\n")
+        linhaImpressaNoArquivo = str(vetorDadosColetados)
+        linhaImpressaNoArquivo = linhaImpressaNoArquivo.replace(',', ' ')
+        linhaImpressaNoArquivo = linhaImpressaNoArquivo.replace('(', ' ')
+        linhaImpressaNoArquivo = linhaImpressaNoArquivo.replace(')', ' ')
+        f.write(linhaImpressaNoArquivo + "\n")
 
-            
-def handlerImu(raw_packet):
+
+def handlerimu(raw_packet):
     filename = 'Imu_' + str(getDateTime()) + '.txt'
     print("Generating output in " + filename)
     with open(filename, 'a') as f:
@@ -35,29 +36,29 @@ def handlerImu(raw_packet):
         f.write(str(packet) + "\n")
 
 
-def getDateTime():
+def getdatetime():
     now = datetime.now()
     date_time = now.strftime("%m%d%Y_%H%M")
     print("Running at", date_time)
     return date_time
 
 
-def startOuster():
+def startouster():
     os1 = OS1('10.5.5.86', '10.5.5.1', mode='1024x10')
     os1.start()
     os1.run_forever(handler)
 
 
-def startOusterImu():
+def startousterimu():
     imu = OS1_IMU('10.5.5.86', '10.5.5.1', mode='1024x10')
     imu.start()
-    imu.run_forever(handlerImu)
+    imu.run_forever(handlerimu)
 
 
 while True:
     hostname = "10.5.5.86"
     response = os.system("ping -c 1 " + hostname)
     if response == 0:
-        startOuster()
-        startOusterImu()
+        startouster()
+        startousterimu()
         break
